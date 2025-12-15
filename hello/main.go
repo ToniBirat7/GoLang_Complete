e4a517e066c94df54c5aee4109fa26c3b4dbc895
Birat <gotofreelancing1@gmail.com>
2025-12-15 13:09:42 +0545

    pointer

--- hello/main.go
+++ hello/main.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 )
 
-func changeNum(num int) {
-	num = 5
-	fmt.Println("In ChangeNum : ", num)
+func changeNum(num *int) {
+	fmt.Println("In ChangeNum : ", *num)
+	*num = 5 // Derefencing
 }
 
 func main() {
 	num := 1
 
-	changeNum(num)
+	changeNum(&num)
 
 	fmt.Println("After ChangeNum : ", num)
 }
