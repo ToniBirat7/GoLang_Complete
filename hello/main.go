1a216f1319f7f46018d4221963f0689c357f7fe2
Birat <gotofreelancing1@gmail.com>
2025-12-15 12:38:12 +0545

    functions

--- hello/main.go
+++ hello/main.go
@@ -6,7 +6,14 @@ func add(a, b int) int {
 	return a + b
 }
 
+func swap(a int, b int) (int, int) {
+	return b, a
+}
+
 func main() {
 	sum := add(2, 3)
 	fmt.Println(sum)
+
+	x, y := swap(2, 3)
+	fmt.Println(x, y)
 }
