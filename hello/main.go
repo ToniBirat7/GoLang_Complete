934cfc2c5f0b4d0e8463a103752865ee0109854d
Birat <gotofreelancing1@gmail.com>
2025-12-15 13:23:52 +0545

    pointer

--- hello/main.go
+++ hello/main.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 type order struct {
-	id        string
-	amount    float32
-	status    string
-	createdAt time.Time // Nanosecond precision
+	id     string
+	amount float32
+	status string
+	// createdAt time.Time // Nanosecond precision
 }
 
 func main() {
-	order := order{
-		id:        "123",
-		amount:    1200.00,
-		status:    "received",
-		createdAt: time.Now(),
+	myOrder := order{
+		id:     "123",
+		amount: 1200.00,
+		status: "received",
+		// createdAt: time.Now(),
 	}
 
-	fmt.Println("Order Struct", order)
+	fmt.Println("Order Struct", myOrder)
 }
