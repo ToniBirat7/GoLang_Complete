0a675f8a8cada331ec0700dd4213be1bbc792228
Birat <gotofreelancing1@gmail.com>
2025-12-15 11:39:42 +0545

    array and slices

--- hello/main.go
+++ hello/main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 3; i++ {
-		fmt.Println(i)
-	}
+	// Array
+	var nums [4]int
+
+	tensor := [2][2]int{{1, 2}, {3, 4}}
+
+	nums[0] = 1
+	nums[2] = 2
+
+	fmt.Println(tensor)
 }
