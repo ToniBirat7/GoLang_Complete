422ead64193d4ef4e8cee1e465c502dcffdd5830
Birat <gotofreelancing1@gmail.com>
2025-12-15 12:41:56 +0545

    functions

--- hello/main.go
+++ hello/main.go
@@ -2,18 +2,15 @@ package main
 
 import "fmt"
 
-func add(a, b int) int {
+func applyOperation(a int, b int, operation func(int, int) int) int {
+	return operation(a, b)
+}
+
+func add(a int, b int) int {
 	return a + b
 }
 
-func swap(a int, b int) (int, int) {
-	return b, a
-}
-
 func main() {
-	sum := add(2, 3)
-	fmt.Println(sum)
-
-	x, y := swap(2, 3)
-	fmt.Println(x, y)
+	result := applyOperation(2, 3, add)
+	fmt.Println(result)
 }
