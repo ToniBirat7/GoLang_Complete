e983f578439d7c9ff36d5f92a367ab8e901575dd
Birat <gotofreelancing1@gmail.com>
2025-12-18 14:31:50 +0545

    go routines

--- hello/main.go
+++ hello/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func task(id int) {
-	fmt.Println("Doing Something", id)
+func blockingTask(id int) {
+	fmt.Printf("Goroutine %d: Starting blocking task\n", id)
+	time.Sleep(2 * time.Second) // Simulating a blocking operation
+	fmt.Printf("Goroutine %d: Completed blocking task\n", id)
 }
 
 func main() {
-	for i := 0; i <= 10; i++ {
-		go task(i)
+	for i := 1; i <= 3; i++ {
+		go blockingTask(i)
 	}
+	time.Sleep(5 * time.Second) // Sleep to allow Goroutines to complete
 }
