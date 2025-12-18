3b679cfeec6e46fe0680bfa9e47a5e50be959535
Birat <gotofreelancing1@gmail.com>
2025-12-18 10:59:07 +0545

    go cont

--- hello/main.go
+++ hello/main.go
@@ -2,36 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-type PaymentMethod interface {
-	ProcessPayment(amount float32) string
-}
-
-type CreditCard struct {
-	cardNumber string
-}
-
-func (cc CreditCard) ProcessPayment(amount float32) string {
-	return fmt.Sprintf("Processing credit card payment of $%.2f using card number %s", amount, cc.cardNumber)
-}
-
-type PayPal struct {
-	email string
-}
-
-func (pp PayPal) ProcessPayment(amount float32) string {
-	return fmt.Sprintf("Processing PayPal payment of $%.2f using email %s", amount, pp.email)
-}
-
-func processPayment(method PaymentMethod, amount float32) {
-	result := method.ProcessPayment(amount)
-	fmt.Println(result)
+type order struct {
+	id        string
+	amount    float32
+	status    string
+	createdAt time.Time
 }
 
 func main() {
-	cc := CreditCard{cardNumber: "1234-5678-9012-3456"}
-	pp := PayPal{email: "user@example.com"}
-	processPayment(cc, 100.00)
-	processPayment(pp, 50.00)
+	myOrder := order{
+		id:     "1",
+		amount: 200.0,
+		status: "no",
+	}
+
+	fmt.Println(myOrder)
 }
