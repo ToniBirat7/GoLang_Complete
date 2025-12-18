2aac0f60287a4531e5151fa65cbf53ce17e012ac
Birat <gotofreelancing1@gmail.com>
2025-12-18 12:06:28 +0545

    go enums

--- hello/main.go
+++ hello/main.go
@@ -1,37 +1,24 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
+
+const (
+	Sunday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
 )
 
-type PaymentMethod interface {
-	ProcessPayment(amount float32) string
-}
-
-type CreditCard struct {
-	cardNumber string
-}
-
-func (cc CreditCard) ProcessPayment(amount float32) string {
-	return fmt.Sprintf("Processing credit card payment of $%.2f using card number %s", amount, cc.cardNumber)
-}
-
-type PayPal struct {
-	email string
-}
-
-func (cc PayPal) ProcessPayment(amount float32) string {
-	return fmt.Sprintf("Processing PayPal card payment of $%.2f using card number %s", amount, cc.email)
-}
-
-func processPayment(method PaymentMethod, amount float32) {
-	result := method.ProcessPayment(amount)
-	fmt.Println(result)
-}
-
 func main() {
-	cc := CreditCard{cardNumber: "1234-5678"}
-	pp := PayPal{email: "hi@"}
-	processPayment(cc, 100.0)
-	processPayment(pp, 50.0)
+	fmt.Println("Days of the week:")
+	fmt.Println("Sunday:", Sunday)
+	fmt.Println("Monday:", Monday)
+	fmt.Println("Tuesday:", Tuesday)
+	fmt.Println("Wednesday:", Wednesday)
+	fmt.Println("Thursday:", Thursday)
+	fmt.Println("Friday:", Friday)
+	fmt.Println("Saturday:", Saturday)
 }
