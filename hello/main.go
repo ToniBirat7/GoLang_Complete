7d4ac8b1a99d24113c64d5699ce61b25154fc872
Birat <gotofreelancing1@gmail.com>
2025-12-18 14:06:43 +0545

    go routines

--- hello/main.go
+++ hello/main.go
@@ -1,30 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Number interface {
-	int | string
-}
-
-type Pair[T any, U any] struct {
-	first  T
-	second U
-}
-
-func printSlice[T Number](items []T) {
-	for _, item := range items {
-		fmt.Println(item)
-	}
+func task(id int) {
+	fmt.Println("Doing Something", id)
 }
 
 func main() {
-	pair := Pair[string, bool]{
-		first:  "birat",
-		second: true,
+	for i := 0; i <= 10; i++ {
+		go task(i)
 	}
-	fmt.Println(pair.first)
-	printSlice([]int{1, 2, 3, 4})
-	printSlice([]string{"1", "2", "3", "4"})
 }
