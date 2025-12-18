210db782adefd927f8e9796ca50469247598de22
Birat <gotofreelancing1@gmail.com>
2025-12-18 13:19:09 +0545

    go generic

--- hello/main.go
+++ hello/main.go
@@ -8,6 +8,11 @@ type Number interface {
 	int | string
 }
 
+type Pair[T any, U any] struct {
+	first  T
+	second U
+}
+
 func printSlice[T Number](items []T) {
 	for _, item := range items {
 		fmt.Println(item)
@@ -15,6 +20,11 @@ func printSlice[T Number](items []T) {
 }
 
 func main() {
+	pair := Pair[string, bool]{
+		first:  "birat",
+		second: true,
+	}
+	fmt.Println(pair.first)
 	printSlice([]int{1, 2, 3, 4})
 	printSlice([]string{"1", "2", "3", "4"})
 }
