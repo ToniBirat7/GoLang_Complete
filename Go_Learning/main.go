1417a6e1222b8b4fc15d186762865fe102fdce0c
Birat <gotofreelancing1@gmail.com>
2025-12-19 14:43:27 +0545

    goroutine channels

--- Go_Learning/main.go
+++ Go_Learning/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func task(id int) {
+func task(id int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println("Doing Task", id)
 }
+
+var wg sync.WaitGroup
 
 func main() {
 
 	for i := 0; i <= 10; i++ {
 		wg.Add(1)
-		go task(i)
+		go task(i, &wg)
 	}
 
 	fmt.Println("Completed")
