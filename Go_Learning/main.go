005fe23d844b07a9b07e1b9d74cfc4b4d846f3f8
Birat <gotofreelancing1@gmail.com>
2025-12-21 07:52:11 +0545

    go mutex

--- Go_Learning/main.go
+++ Go_Learning/main.go
@@ -1,34 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-// It is better to pass WaitGroup by pointer or use a global one
-var wg sync.WaitGroup
-
-func receiver(numChan chan int) {
-	// 4. This decrements the counter when the function finishes
-	defer wg.Done()
-
-	val := <-numChan
-	fmt.Printf("Goroutine received: %d\n", val)
+type post struct {
+	views int
 }
 
-func sender(value int, messageChan chan int) {
-	messageChan <- 5
+func (p *post) inc() {
+	p.views += 1
 }
 
 func main() {
-	messageChan := make(chan int)
+	myPost := post{
+		views: 0,
+	}
 
-	wg.Add(1)
-	go receiver(messageChan)
-
-	wg.Add(1)
-	go sender(5, messageChan)
-
-	wg.Wait()
-	fmt.Println("Main: All tasks finished.")
+	myPost.inc()
 }
