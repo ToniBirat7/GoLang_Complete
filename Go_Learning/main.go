4a6219f82c43cf6ef3e5ae0b0076bf8539d25e0f
Birat <gotofreelancing1@gmail.com>
2025-12-21 07:25:22 +0545

    channels in go

--- Go_Learning/main.go
+++ Go_Learning/main.go
@@ -5,19 +5,27 @@ import (
 	"sync"
 )
 
+// It is better to pass WaitGroup by pointer or use a global one
 var wg sync.WaitGroup
 
-func task(numChan chan int) {
-	fmt.Println("Received Value is : ", <-numChan)
+func task(id int, numChan chan int) {
+	// 4. This decrements the counter when the function finishes
+	defer wg.Done()
+
+	val := <-numChan
+	fmt.Printf("Goroutine %d received: %d\n", id, val)
 }
 
 func main() {
-
 	messageChan := make(chan int)
 
-	go task(messageChan)
+	// --- FIRST TASK ---
+	wg.Add(1) // 1. Increment counter BEFORE starting goroutine
+	go task(1, messageChan)
 
-	messageChan <- 5
+	messageChan <- 5 // 2. Send value (blocks until G1 is ready to receive)
 
+	// 6. Now Wait() will block because the counter is 2
 	wg.Wait()
+	fmt.Println("Main: All tasks finished.")
 }
