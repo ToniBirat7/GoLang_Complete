672362e2e3937a249b656e1231dae3abf6b84619
Birat <gotofreelancing1@gmail.com>
2025-12-21 07:57:16 +0545

    mutex

--- Go_Learning/main.go
+++ Go_Learning/main.go
@@ -1,17 +1,27 @@
 package main
 
+import "fmt"
+
 type post struct {
 	views int
 }
 
 func (p *post) inc() {
 	p.views += 1
+	fmt.Println("Current Views is : ", p.views)
+}
+
+func callInc(myPost post) {
+	myPost.inc()
 }
 
 func main() {
+
 	myPost := post{
 		views: 0,
 	}
 
-	myPost.inc()
+	for i := 0; i <= 10; i++ {
+		go callInc(myPost)
+	}
 }
