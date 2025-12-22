d44ce3406f23b1a8ff2a04c24d08af6104ade059
Birat <gotofreelancing1@gmail.com>
2025-12-22 11:47:46 +0545

    go mutex

--- Go_Learning/main.go
+++ Go_Learning/main.go
@@ -7,12 +7,18 @@ import (
 
 type post struct {
 	views int
+	mu    sync.Mutex
 }
 
 var wg sync.WaitGroup
 
 func (p *post) inc(wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer func() {
+		p.mu.Unlock()
+		wg.Done()
+	}()
+
+	p.mu.Lock()
 	p.views += 1
 }
 
