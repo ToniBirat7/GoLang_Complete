dcee7c45aeff6b6a93409691af87414c098b9e84
Birat <gotofreelancing1@gmail.com>
2025-12-22 14:45:52 +0545

    rest api in go

--- Go_Learning/restapi/internal/config/config.go
+++ Go_Learning/restapi/internal/config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type HTTPServer struct {
 	Addr string
 }
@@ -11,5 +19,30 @@ type Config struct {
 }
 
 func MustLoad() {
-	
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the config file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("cannot read config file: %s", err.Error())
+	}
 }
