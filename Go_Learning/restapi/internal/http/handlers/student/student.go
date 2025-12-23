05e5ee3c6e1b7b76ca69db59d2f3b5bb27cc5684
Birat <gotofreelancing1@gmail.com>
2025-12-23 12:14:47 +0545

    accepting POST

--- Go_Learning/restapi/internal/http/handlers/student/student.go
+++ Go_Learning/restapi/internal/http/handlers/student/student.go
@@ -1,13 +1,27 @@
 package student
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"log/slog"
 	"net/http"
+
+	"github.com/birat/restapi/internal/types"
 )
 
 func NewStudent() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		var student types.Student
+
+		err := json.NewDecoder(r.Body).Decode(&student)
+
+		if errors.Is(err, io.EOF) {
+			// JSON Response
+		}
+
 		slog.Info("creating a student")
-		w.Write([]byte("Welcome to Student API"))
+
 	}
 }
