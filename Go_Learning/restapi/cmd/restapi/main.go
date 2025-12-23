0c7bd8b38cb904acc06f4d12b22e38c731d7d133
Birat <gotofreelancing1@gmail.com>
2025-12-23 11:13:50 +0545

    package completed

--- Go_Learning/restapi/cmd/restapi/main.go
+++ Go_Learning/restapi/cmd/restapi/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/birat/restapi/internal/config"
 )
@@ -24,10 +27,21 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Printf("Server Started %s\n", cfg.HTTPServer.Addr)
-	err := server.ListenAndServe()
+	done := make(chan os.Signal, 1)
 
-	if err != nil {
-		log.Fatalf("failed to start server")
-	}
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		fmt.Printf("Server Started %s\n", cfg.HTTPServer.Addr)
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatalf("failed to start server")
+		}
+	}()
+
+	// Blocking, Until the Channel is not Notified by Signal
+	<-done
+
+	// Code runs as usual 
 }
