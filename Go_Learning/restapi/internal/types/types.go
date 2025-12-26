c06fb4c1e7ef8dadba73b8726bdea4f665209220
Birat <gotofreelancing1@gmail.com>
2025-12-26 11:28:39 +0545

    validate post request

--- Go_Learning/restapi/internal/types/types.go
+++ Go_Learning/restapi/internal/types/types.go
@@ -3,6 +3,6 @@ package types
 type Student struct {
 	Id    int
 	Name  string `validate:"required"`
-	Email string `validate:"required"`
+	Email string `validate:"required,email"`
 	Age   int    `validate:"required"`
 }
